79cb393a7f979ff3db7c7e789ecb6e3be391643c
waffeln <nils@kaster.space>
2023-01-29 16:40:07 +0100

    adjust prepareDataByYear return structure

--- src/FetchAndPrepareUrlData.ts
+++ src/FetchAndPrepareUrlData.ts
@@ -32,14 +32,15 @@ const parseCsvToJson = (data: string) => {
 const prepareDataByYear = (data: PrepareDataByYearType[][]): PrepareDataByYearType => {
 	const result = {};
 	const keyName = "year";
+	const personKeyName = "politician";
 	data.forEach((urlElement) => {
 		urlElement.forEach((el) => {
 			if (el[keyName] === undefined) return;
 			const year = el[keyName].substring(0, 4);
-			if (result[year] === undefined) result[year] = {};
+			if (result[year] === undefined) result[year] = {[result[year][personKeyName]]: {}};
 			Object.keys(el).forEach((el2) => {
-				if (result[year][el2] === undefined) result[year][el2] = 0;
-				if (el2 !== keyName) result[year][el2] += 1;
+				if (result[year][personKeyName][el2] === undefined) result[year][personKeyName][el2] = 0;
+				if (el2 !== keyName && el2 !== personKeyName) result[year][personKeyName][el2] += 1;
 			});
 		});
 	});
