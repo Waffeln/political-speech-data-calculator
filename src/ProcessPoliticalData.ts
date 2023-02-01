c0f875f776f288c21a5202c03b1e8fa35db11fa9
waffeln <nils@kaster.space>
2023-02-01 14:25:31 +0100

    cleanup

--- src/ProcessPoliticalData.ts
+++ src/ProcessPoliticalData.ts
@@ -6,14 +6,14 @@ interface PoliticianCounter {
 
 const politicianKeyName = "Speaker";
 
-const getPoliticianWithMostSpeechesByKey = (data, keyName: string, keyValue: string | number): string | null => {
+const getPoliticianWithMostSpeechesByKey = (data, keyName: string, expectedValue: string | number): string | null => {
 	const politicianCounter: PoliticianCounter = {};
 
 	data.forEach((urlElement) => {
 		urlElement.forEach((el) => {
 			if (moment(el[keyName], "YYYY-MM-DD", true).isValid()) {
-				if (el[keyName].substring(0, 4) !== keyValue) return;
-			} else if (el[keyName] !== keyValue) return;
+				if (el[keyName].substring(0, 4) !== expectedValue) return;
+			} else if (el[keyName] !== expectedValue) return;
 			if (politicianCounter[el[politicianKeyName]] === undefined) {
 				politicianCounter[el[politicianKeyName]] = 0;
 			}
@@ -21,8 +21,8 @@ const getPoliticianWithMostSpeechesByKey = (data, keyName: string, keyValue: str
 		});
 	});
 
-	const highestVal = Math.max(...Object.values(politicianCounter));
-	const result = Object.keys(politicianCounter).filter(key => politicianCounter[key] === highestVal);
+	const highestValue = Math.max(...Object.values(politicianCounter));
+	const result = Object.keys(politicianCounter).filter(key => politicianCounter[key] === highestValue);
 
 	if (result.length !== 1) return null;
 	return result[0];
@@ -39,8 +39,8 @@ const getPoliticianWithLowestKeyValue = (data, keyName: string): string | null =
 		});
 	});
 
-	const lowestVal = Math.min(...Object.values(politicianCounter));
-	const result = Object.keys(politicianCounter).filter(key => politicianCounter[key] === lowestVal);
+	const lowestValue = Math.min(...Object.values(politicianCounter));
+	const result = Object.keys(politicianCounter).filter(key => politicianCounter[key] === lowestValue);
 
 	if (result.length !== 1) return null;
 	return result[0];
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import FetchAndPrepareUrlData from "./FetchUrlData";
+import FetchUrlData from "./FetchUrlData";
 import ProcessData from "./ProcessPoliticalData";
 import express from "express";
 
@@ -7,15 +7,13 @@ app.listen(5000);
 
 app.get("/evaluation", async (request, response) => {
 	const requestArray = typeof request.query.url === "object" ? request.query.url : [request.query.url];
-	const data = FetchAndPrepareUrlData.fetchCsvOverUrl(requestArray as string[]);
+	const data = FetchUrlData.fetchCsvOverUrl(requestArray as string[]);
 
-	const result = await Promise.all(data).then((data) => {
-		const result = {};
-		result["mostSpeeches"] = ProcessData.getPoliticianWithMostSpeechesByKey(data, "Date", "2013");
-		result["mostSecure"] = ProcessData.getPoliticianWithMostSpeechesByKey(data, "Topic", "Internal Security");
-		result["leastWordy"] = ProcessData.getPoliticianWithLowestKeyValue(data, "Words");
-		return result;
-	});
+	const result = await Promise.all(data).then((data) => ({
+		mostSpeeches: ProcessData.getPoliticianWithMostSpeechesByKey(data, "Date", "2013"),
+		mostSecure: ProcessData.getPoliticianWithMostSpeechesByKey(data, "Topic", "Internal Security"),
+		leastWordy: ProcessData.getPoliticianWithLowestKeyValue(data, "Words")
+	}));
 	response.json(result);
 });
 
